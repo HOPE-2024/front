79bc1a4e0f4a8e2325dec2d23730d71c6f5dfc5b
HanbiHeo <hhb0365@gmail.com>
2024-01-22 10:13:03 +0900

    first commit

---
+++ src/api/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const storage = firebase.storage();
