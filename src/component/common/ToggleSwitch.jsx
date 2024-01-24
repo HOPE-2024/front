f8d6a91b983375e75f851ac82d93841a7ee08253
HanbiHeo <hhb0365@gmail.com>
2024-01-24 16:48:09 +0900

    토글 지역리스트 라인버튼

--- src/component/common/ToggleSwitch.jsx
+++ src/component/common/ToggleSwitch.jsx
@@ -4,32 +4,39 @@ import styled from "styled-components";
 export const ToggleContainer = styled.div`
   position: relative;
   margin-top: 1rem;
-  left: 47%;
+  left: 46%;
   cursor: pointer;
 
   > .toggle-container {
-    width: 52px;
-    height: 26px;
+    width: 88px;
+    height: 30px;
     border-radius: 30px;
-    background-color: var(--SKY);
+    background-color: ${(props) => props.backgroundColor || "var(--SKY)"};
   }
   > .toggle--checked {
-    background-color: var(--BLUE);
+    background-color: ${(props) => props.backgroundColor || "var(--NAVY)"};
     transition: 0.5s;
   }
   > .toggle-circle {
     position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 0.9rem;
     top: 2px;
-    left: 2px;
-    width: 22px;
-    height: 22px;
-    border-radius: 50%;
-    background-color: var(--WHITE);
+    left: 3px;
+    width: 40px;
+    height: 25px;
+    border-radius: 30px;
+    background-color: ${(props) => props.backgroundColor || "var(--WHITE)"};
     transition: 0.5s;
   }
   > .toggle--checked {
-    left: 27px;
+    left: 45px;
     transition: 0.5s;
+  }
+  > .toggle-circle-text {
+    font-size: 0.5rem;
   }
 `;
 
@@ -51,7 +58,9 @@ export const Toggle = () => {
         <div
           className={`toggle-container ${isOn ? "toggle--checked" : null}`}
         />
-        <div className={`toggle-circle ${isOn ? "toggle--checked" : null}`} />
+        <div className={`toggle-circle ${isOn ? "toggle--checked" : null}`}>
+          <span className="toggle-circle-text">{isOn ? "지역" : "증상"}</span>
+        </div>
       </ToggleContainer>
     </>
   );
