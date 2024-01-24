ce1c4871443879717e468e428f3655debec28017
ydj1215 <ydjdj2@naver.com>
2024-01-24 13:50:34 +0900

    240124 1350 ydj

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Login } from "./pages/member/Login";
 import { ChatList } from "./pages/chat/ChatList";
 import { Home } from "./pages/Home";
-import { Template } from "./component/Template/Template";
+import { Template } from ".//Template/Template";
 import { Map } from "./pages/map/Map";
 import { MemberList } from "./pages/admin/MemberList";
 import { Report } from "./pages/admin/Report";
