cd6578d6d60ee57295b1d0e7f66c9b94bb208220
songwoohee <songwoohee@naver.com>
2024-02-01 18:05:23 +0900

    agreeCheck 모달 추가(구현중)

--- src/pages/agreeToTerms/AgreeCheck.jsx
+++ src/pages/agreeToTerms/AgreeCheck.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { SubHeader } from "../../component/template/SubHeader";
 import {
   Container,
@@ -25,12 +25,10 @@ export const AgreeCheck = () => {
   const [useTermCheck, setUseTermCheck] = useState(false);
   const [privacyCheck, setPrivacyCheck] = useState(false);
   const [marketingCheck, setMarketingCheck] = useState(false);
-  // 팝업 처리
-  const [madalOpen, setModalOpen] = useState(false);
-
-  const closeModal = () => {
-    setModalOpen(false);
-  };
+  // 약관 내용 보기 모달
+  const [useTermModalOpen, setUseTermModalOpen] = useState(false);
+  const [privacyModalOpen, setPrivacyModalOpen] = useState(false);
+  const [marketingModalOpen, setMarketingModalOpen] = useState(false);
 
   // 전체 동의 체크박스를 클릭했을 때 모든 항목의 체크 상태 변경
   const hadleAllCheck = () => {
@@ -69,17 +67,19 @@ export const AgreeCheck = () => {
   };
 
   // 이용약관, 개인정보 수집, 마케팅 3가지 모두 선택되면 전체 자동 체크
-  const updateAllCheck = () => {
+  // useCallback : 함수가 필요한 시점(의존값 변경)에만 새로 생성, 다른 시점에는 기존 함수 재사용.
+  // 이를 통해 useEffect내에서 매 랜더링마다 함수가 새로 생성되는것을 방지 -> 성능 최적화 효과
+  const updateAllCheck = useCallback(() => {
     if (useTermCheck && privacyCheck && marketingCheck) {
       setAllCheck(true);
     } else {
       setAllCheck(false);
     }
-  };
+  }, [useTermCheck, privacyCheck, marketingCheck, setAllCheck]);
 
   useEffect(() => {
     updateAllCheck();
-  }, [useTermCheck, privacyCheck, marketingCheck]);
+  }, [useTermCheck, privacyCheck, marketingCheck, updateAllCheck]);
 
   // 필수 약관 체크 안하면 알림창 뜨기
   const handleNextButtonClick = () => {
@@ -93,6 +93,40 @@ export const AgreeCheck = () => {
   // 약관 체크 후 다음 페이지로 이동
   const nextPage = () => {
     navigate("/signup");
+  };
+
+  // 각 약관에 대한 모달 오픈
+  const openModal = (modalType) => {
+    switch (modalType) {
+      case "usetermCheck":
+        setUseTermModalOpen(true);
+        break;
+      case "privacyCheck":
+        setPrivacyModalOpen(true);
+        break;
+      case "marketingCheck":
+        setMarketingModalOpen(true);
+        break;
+      default:
+        break;
+    }
+  };
+
+  // 각 약관에 대한 모달 닫기
+  const closeModal = (modalType) => {
+    switch (modalType) {
+      case "usetermCheck":
+        setUseTermModalOpen(false);
+        break;
+      case "privacyCheck":
+        setPrivacyModalOpen(false);
+        break;
+      case "marketingCheck":
+        setMarketingModalOpen(false);
+        break;
+      default:
+        break;
+    }
   };
 
   return (
@@ -166,6 +200,25 @@ export const AgreeCheck = () => {
           </Button>
         </ButtonContainer>
       </Container>
+      {/* 각 약관에 대한 모달 */}
+      <YesModal
+        isOpen={useTermModalOpen}
+        closeModal={() => closeModal("useTerm")}
+      >
+        {/* 이용약관 내용 */}
+      </YesModal>
+      <YesModal
+        isOpen={privacyModalOpen}
+        closeModal={() => closeModal("privacy")}
+      >
+        {/* 개인정보 수집 및 이용 내용 */}
+      </YesModal>
+      <YesModal
+        isOpen={marketingModalOpen}
+        closeModal={() => closeModal("marketing")}
+      >
+        {/* 광고성 정보 수신 동의 내용 */}
+      </YesModal>
     </>
   );
 };
