4d335aaa4a945bfb98f18891d2e9bac7fbf37633
HyunSu1210 <hyunsu015@gmail.com>
2024-02-15 11:34:07 +0900

    실시간 검색어 기능 구현

--- src/component/mainSlideElement/ElasticSearch.jsx
+++ src/component/mainSlideElement/ElasticSearch.jsx
@@ -1,9 +1,34 @@
 import { WordSwitcher } from "../../css/text/WordSwitcher";
 import { StyledSearch } from "../../css/common/StyledSearch";
 import { useNavigate } from "react-router-dom";
+import { Ranking } from "../search/Ranking";
+import { useEffect, useState } from "react";
+import { MedicineDataAxiosApi } from "../../api/MedicineDataAxiosApi";
+import styled from "styled-components";
 
+const RankingBox = styled.div`
+  position: absolute;
+  top: 54%;
+  left: 42%;
+`;
 export const ElasticSearch = () => {
   const navigate = useNavigate();
+  const [ranking, setRanking] = useState([]);
+
+  // 검색어 집계
+  useEffect(() => {
+    const getRanking = async () => {
+      const rsp = await MedicineDataAxiosApi.getSearchLog();
+      if (rsp) {
+        console.log("검색어 순위 : ", rsp.data);
+        setRanking(rsp.data);
+      } else {
+        console.log("검색어 불러오지 못함");
+      }
+    };
+    getRanking();
+  }, []);
+
   const onSearch = (searchOption, search) => {
     navigate(`/medicineresult?searchoption=${searchOption}&search=${search}`);
   };
@@ -11,6 +36,9 @@ export const ElasticSearch = () => {
     <>
       <WordSwitcher></WordSwitcher>
       <StyledSearch onSearch={onSearch}></StyledSearch>
+      <RankingBox>
+        <Ranking data={ranking}></Ranking>
+      </RankingBox>
     </>
   );
 };
