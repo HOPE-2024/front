03360085f52f17685c6a7a9ed37c178353a6a381
songwoohee <songwoohee@naver.com>
2024-02-15 15:54:50 +0900

    header dropdown 계정에 탈퇴 추가

--- src/component/template/HeaderDropDown.jsx
+++ src/component/template/HeaderDropDown.jsx
@@ -155,6 +155,13 @@ export const ThirdDropDown = ({ onClose }) => {
         이용약관
       </li>
       <li onClick={logout}>로그아웃</li>
+      <li
+        onClick={() => {
+          navigate("/withdraw");
+        }}
+      >
+        회원탈퇴
+      </li>
     </ThirdDropDownMenu>
   );
 };
