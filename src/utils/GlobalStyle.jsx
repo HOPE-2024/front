f1fb89233baa0af9204c3b434a32378c88dd4ca6
songwoohee <songwoohee@naver.com>
2024-02-16 11:50:59 +0900

    globalstyle font 재적용

--- src/utils/GlobalStyle.jsx
+++ src/utils/GlobalStyle.jsx
@@ -3,19 +3,14 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-
-  /* @font-face {
-     font-family: 'DungGeunMo';
-     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/DungGeunMo.woff') format('woff');
-     font-weight: normal;
-     font-style: normal;
-  } */
+  
   @font-face {
     font-family: 'SUIT-Regular';
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
     font-weight: normal;
     font-style: normal;
 }
+
 
   :root {
     --BLUE : #136CFB ;  
